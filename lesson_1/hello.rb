98844b0d762aba32cd2c787d476c94874d216d0d
Bis87 <pavel.rytvinsky@gmail.com>
2016-10-12 22:02:03 +0300

    dice no cycle

--- lesson_1/hello.rb
+++ lesson_1/hello.rb
@@ -1,7 +1,3 @@
-current_path = File.dirname(__FILE__)
-
 require_relative 'requires/hello_method.rb'
-
-
 
 hello
---
+++ lesson_2/dice.rb
@@ -0,0 +1,7 @@
+puts "how many dice?"
+
+number = gets.to_i
+
+number.times do
+  puts rand(6) + 1
+end
