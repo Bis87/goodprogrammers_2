20f7fc68b59a4f103e72f252cd66e54d26bc6977
Bis87 <pavel.rytvinsky@gmail.com>
2016-10-12 22:16:36 +0300

    dice with score counter

--- lesson_2/dice.rb
+++ lesson_2/dice.rb
@@ -2,6 +2,13 @@ puts "how many dice?"
 
 number = gets.to_i
 
+dice_summ = 0
+
 number.times do
-  puts rand(6) + 1
+  dice = rand(6) + 1
+  puts dice
+
+  dice_summ += dice
 end
+
+puts "Your scores: #{dice_summ}"
